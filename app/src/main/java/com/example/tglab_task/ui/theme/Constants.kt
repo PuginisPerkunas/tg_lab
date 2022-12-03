6ec958470ccca6b5727d5afbfef0639ba1b44c69
PuginisPrkunas <yzyzz852@gmail.com>
2022-12-03 20:17:53 +0200

    init commit;

---
+++ app/src/main/java/com/example/tglab_task/ui/theme/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.tglab_task.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val VERTICAL_SPACER_HEIGHT = 12.dp
+
+val HORIZONTAL_CONTENT_PADDING_DP = 18.dp
+val VERTICAL_CONTENT_PADDING_DP = 24.dp
+
+val DATA_CARD_ELEVATION = 0.dp
+val DATA_CARD_CONTENT_PADDING = 12.dp
+
+val DATA_HEADER_CONTENT_PADDING = 12.dp
